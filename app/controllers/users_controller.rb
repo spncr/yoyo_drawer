78709b545d720a8ceb809849d3904a55f62ef5a6
spncr <spencerwberry@gmail.com>
2019-05-30 13:20:05 -0700

    Locked user creation and linked title back to index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  #skip_before_action :require_login, only: [:index, :new, :create]
+  skip_before_action :require_login, only: [:index, :new, :create]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   # GET /users
   # GET /users.json
