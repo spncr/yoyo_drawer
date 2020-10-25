3cea6fdd4a4c080b4a52fd37d5bb7571d3581f23
spncr <spencerwberry@gmail.com>
2020-10-25 13:49:43 -0700

    Readme updated with testing instructions 🤞🏻

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # README
 
-This is a Ruby on Rails Project designed to inventory my yoyo collection in the most basic way.
+This is a Ruby on Rails Project designed to inventory my yoyo collection for public display and organization.
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## GOALS : DID I MEET them
+## How does it work?
+## Next features
+* Add a new row for 'name' which exists in addition to 'model'
 
-Ruby 2.6.1
-Rails 5.2.3
+## Run this project locally
 
-Removed Sass and CoffeeScript on purpose, because I'ma noob.
+1. [Rails](https://guides.rubyonrails.org/getting_started.html) 5.2.3 is required, which has some prerequisites:
+ * Ruby (2.7.2 or later)
+ * Node.js
+ * Yarn
+2. [PostgreSQL](https://www.postgresql.org/download/) is also required, which is a Heroku requirement.
+3. [Rbenv](https://github.com/rbenv/rbenv) manages the environment, to ensure you're running the proper version of Ruby.
+4. Now you are ready to clone the project:
+`git clone git@github.com:spencerberry/yoyo_drawer.git`
+5. Bundler will install all the gems for you with `bundle install` (be sure to `cd` into the project first)
+6. Next we need the database to be running, so we can create a new user for this app. `CREATE USER yoyos WITH PASSWORD 'plzwork' CREATEDB CREATEROLE LOGIN SUPERUSER;` is how the app is configured, but you should create your own username and password and set it in `yoyos/config/database.yml`
+7. Once this user is working, you can `rails db:create` to create the databases themselves and `rails db:migrate` to fill them with the models.
+8. You should now have all the dependencies installed and the migrated database running, so you can `rails server` and visit the local route to see the development app: [http://127.0.0.1:3000](http://127.0.0.1:3000)
+
+Feedback encouraged!
--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 2019_05_17_221209) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", id: :bigint, default: nil, force: :cascade do |t|
-    t.text "name", null: false
-    t.text "record_type", null: false
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -25,30 +25,20 @@ ActiveRecord::Schema.define(version: 2019_05_17_221209) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", id: :bigint, default: nil, force: :cascade do |t|
-    t.text "key", null: false
-    t.text "filename", null: false
-    t.text "content_type"
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.text "checksum", null: false
+    t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "collectors", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "email", null: false
-    t.string "crypted_password", null: false
-    t.string "salt", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_collectors_on_email", unique: true
-  end
-
-  create_table "comments", id: :bigint, default: nil, force: :cascade do |t|
-    t.text "author_name"
-    t.text "author_email"
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.string "author_email"
     t.text "content"
     t.bigint "yoyo_id"
     t.datetime "created_at", null: false
@@ -56,14 +46,14 @@ ActiveRecord::Schema.define(version: 2019_05_17_221209) do
     t.index ["yoyo_id"], name: "index_comments_on_yoyo_id"
   end
 
-  create_table "makers", id: :bigint, default: nil, force: :cascade do |t|
-    t.text "name"
+  create_table "makers", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "models", id: :bigint, default: nil, force: :cascade do |t|
-    t.text "name"
+  create_table "models", force: :cascade do |t|
+    t.string "name"
     t.bigint "maker_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -80,8 +70,8 @@ ActiveRecord::Schema.define(version: 2019_05_17_221209) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  create_table "yoyos", id: :bigint, default: nil, force: :cascade do |t|
-    t.text "title"
+  create_table "yoyos", force: :cascade do |t|
+    t.string "title"
     t.text "description"
     t.boolean "accepting_offers"
     t.binary "image"
@@ -91,6 +81,8 @@ ActiveRecord::Schema.define(version: 2019_05_17_221209) do
     t.index ["model_id"], name: "index_yoyos_on_model_id"
   end
 
-  add_foreign_key "comments", "yoyos", name: "comments_yoyo_id_fkey"
-  add_foreign_key "models", "makers", name: "models_maker_id_fkey"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "yoyos"
+  add_foreign_key "models", "makers"
+  add_foreign_key "yoyos", "models"
 end
