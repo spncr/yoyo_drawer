686e37134e7e9a4a55b368955e22400019f0706d
spncr <spencerwberry@gmail.com>
2020-10-27 14:17:31 -0700

    Updated a few views to remove unecessary 'back' buttons, cleaned up user states and corresponding footer actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :require_login, only: [:new]
+  #before_action :require_login, only: [:new]
 
-  private
-  def not_authenticated
-    redirect_to login_path, notice: "Please log in first."
-  end
+  # private
+  # def not_authenticated
+  #    redirect_to login_path, notice: "Please log in first."
+  # end
 end
